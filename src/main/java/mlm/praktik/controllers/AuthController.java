d73ebfbe31cce1212275d3f74a7e66fbcbea5120
Joel Bech <joel.bch@hotmail.se>
2025-03-31 15:04:58 +0200

    Small changes

--- src/main/java/mlm/praktik/controllers/AuthController.java
+++ src/main/java/mlm/praktik/controllers/AuthController.java
@@ -6,27 +6,44 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import mlm.praktik.services.AuthService;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 @Controller("/auth")
 public class AuthController {
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @Get("/token")
-    public Map<String, Object> token(HttpRequest<?> request, Optional<io.micronaut.security.authentication.Authentication> authentication) {
+    public Map<String, String> token(HttpRequest<?> request) {
         String authHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION);
 
-        if (authentication.isPresent()) {
-            logger.info("Authenticated user: {}", authentication.get().getAttributes());
-            return authentication.get().getAttributes();
-        } else if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            logger.warn("No token provided");
-            return Map.of("message", "No token provided");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String idTokenString = authHeader.substring(7);
+            try {
+                Map<String, String> response = authService.validateToken(idTokenString);
+                if (response.containsKey("message") && response.get("message").equals("Invalid ID token")) {
+                    logger.warn("Invalid ID token");
+                    response.put("message", "The provided token is invalid.");
+                }
+                return response;
+            } catch (Exception e) {
+                logger.error("Token validation failed: ", e);
+                Map<String, String> errorResponse = new HashMap<>();
+                errorResponse.put("message", "Token validation error: " + e.getMessage());
+                return errorResponse;
+            }
         } else {
-            logger.warn("Invalid or expired token");
-            return Map.of("message", "Invalid or expired token");
+            logger.warn("No valid token found in the request");
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "No valid token found.");
+            return response;
         }
     }
 }
